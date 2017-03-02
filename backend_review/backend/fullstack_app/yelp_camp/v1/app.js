a187bfc6b21ee6d786348c83723bb762458b52eb
bryan buitrago <bryanbuitrago86@gmail.com>
2017-03-01 21:43:57 -0500

    post route working

--- backend_review/backend/fullstack_app/yelp_camp/v1/app.js
+++ backend_review/backend/fullstack_app/yelp_camp/v1/app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -11,10 +13,21 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', (req, res) => {
   var campgrounds = [
     {name: 'Salmon Creek', image: 'http://cdn-jpg2.theactivetimes.net/sites/default/files/camping.jpg'},
-    {name: 'Granite Hill', image: 'http://travelchannel.sndimg.com/content/dam/images/travel/fullrights/2016/01/14/national-park-camping/camping-voyageurs-national-park-tent.jpg.rend.tccom.1280.960.jpeg'},
-    {name: 'Mountain Goat\'s Rest', image:'http://travelchannel.sndimg.com/content/dam/images/travel/fullrights/2016/01/14/national-park-camping/camping-glacier-national-park-camping.jpg.rend.tccom.1280.960.jpeg'}
+    {name: 'Granite Hill', image: 'http://i742.photobucket.com/albums/xx62/wanderingSoul_photos/camping_kashmir.jpg'},
+    {name: 'Mountain Goat\'s Rest', image:'https://www.mindmeister.com/images/download/3047364'}
   ];
     res.render('campgrounds', { campgrounds });
+});
+
+app.post('/campgrounds', (req, res) => { // route to create a new campground
+  console.log(req.body);
+  res.send('you hit the POST route');
+  // get data from form and add to campgrounds array
+  // redirect back to campgrounds page
+});
+
+app.get('/campgrounds/new', (req, res) => { //shows the form that sends the data to the post route
+  res.render('new');
 });
 
 app.listen(port, ()=> {
