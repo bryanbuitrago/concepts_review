392b70ddc4420b96b0ebea7bb451c950297f052f
bryan buitrago <bryanbuitrago86@gmail.com>
2017-03-06 18:09:43 -0500

    Description section added to schema

--- backend_review/backend/fullstack_app/yelp_camp/v2/app.js
+++ backend_review/backend/fullstack_app/yelp_camp/v2/app.js
@@ -11,29 +11,32 @@ app.set('view engine', 'ejs');
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Campground = mongoose.model('Campground', campgroundSchema);
 
-// Campground.create(
-//   {
-//     name: 'Granite Hill',
-//     image: 'http://i742.photobucket.com/albums/xx62/wanderingSoul_photos/camping_kashmir.jpg'
-//   }, function(err, campground) {
-//     if(err) {
-//       console.log(err);
-//     } else {
-//       console.log('NEWLY CREATED CAMPGROUND: ');
-//       console.log(campground);
-//     }
-//   });
+Campground.create(
+  {
+    name: 'Granite Hill',
+    image: 'http://i742.photobucket.com/albums/xx62/wanderingSoul_photos/camping_kashmir.jpg',
+    description: 'This is a huge granite hill, no bathrooms. No water. Beautiful granite!'
+  }, function(err, campground) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log('NEWLY CREATED CAMPGROUND: ');
+      console.log(campground);
+    }
+  });
 
 
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
+// INDEX ROUTE --- SHOW ALL CAMPGROUNDS
 app.get('/campgrounds', (req, res) => {
     // Get all campgrounds from DB
     Campground.find({}, (err, allCampgrounds) => {
@@ -45,7 +48,8 @@ app.get('/campgrounds', (req, res) => {
     });
 });
 
-app.post('/campgrounds', (req, res) => { // route to create a new campground
+// CREATE  --- ADD NEW CAMPGROUND TO DB
+app.post('/campgrounds', (req, res) => {
   // console.log(req.body);
   // get data from form and add to campgrounds array
   var name = req.body.name;
@@ -61,10 +65,16 @@ app.post('/campgrounds', (req, res) => { // route to create a new campground
   });
 });
 
+// NEW --- SHOW FORM TO CREATE NEW CAMPGROUND
 app.get('/campgrounds/new', (req, res) => { //shows the form that sends the data to the post route
   res.render('new');
 });
-
+// SHOW --- SHOWS MORE INFO ABOUT ONE CAMPGROUND
+app.get('/campgrounds/:id', (req, res) => {
+  // find the campground with provided ID
+  // render show template with that campground
+  res.send('THIS WILL BE THE SHOWPAGE ON DAY!!');
+});
 app.listen(port, ()=> {
   console.log(`listening on port ${port}!`);
 });
