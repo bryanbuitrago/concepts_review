a91d80cde81dcafc549c92714a66eb8fe1fd63fa
bryan buitrago <bryanbuitrago86@gmail.com>
2017-03-08 17:43:01 -0500

    Show route /blogs/:id completed

--- backend_review/backend/restful_routes_review/my_restful_blog/app.js
+++ backend_review/backend/restful_routes_review/my_restful_blog/app.js
@@ -23,7 +23,7 @@ var Blog = mongoose.model('Blog', blogSchema);
 // Blog.create({
 //   title: 'Test Blog',
 //   image: 'http://r.ddmcdn.com/s_f/o_1/cx_633/cy_0/cw_1725/ch_1725/w_720/APL/uploads/2014/11/too-cute-doggone-it-video-playlist.jpg',
-//   body: 'HELLO, THIS IS A BLOG POST!'
+//   body: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat'
 // });
 
 // RESTFUL ROUTES
@@ -58,7 +58,19 @@ app.post('/blogs', (req, res) => {
       res.redirect('/blogs');
     }
   });
-})
+});
+
+// SHOW ROUTE
+app.get('/blogs/:id', (req, res) => {
+  // res.send('SHOW PAGE');
+  Blog.findById(req.params.id , (err, foundBlog) => {
+    if(err) {
+        res.redirect('/blogs');
+    } else {
+        res.render('show', {blog: foundBlog});
+    }
+  })
+});
 
 
 app.listen(port, ()=> {
