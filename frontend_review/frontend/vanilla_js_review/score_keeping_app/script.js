ad6fcd8f0f97875711e29945d10b4bc534fe6219
bryan buitrago <bryanbuitrago86@gmail.com>
2017-06-01 14:46:11 -0400

    score keepin app

---
+++ frontend_review/frontend/vanilla_js_review/score_keeping_app/script.js
@@ -0,0 +1,6 @@
+var p1button = document.querySelector('#p1');
+var p2button = document.getElementById('p2');
+
+p1button.addEventListener('click', ()=> console.log('hey p1!'));
+
+p2button.addEventListener('click', () => console.log('hey p2!'));
